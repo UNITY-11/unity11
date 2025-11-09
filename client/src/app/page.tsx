0d1a1f67627fabfe1bad3537442c42d085e7a9ef
ajmal faris <ajmalfaris.me@gmail.com>
2025-11-09 20:29:46 +0530

    feat (home) : add the Clients Section in the home page

--- client/src/app/page.tsx
+++ client/src/app/page.tsx
@@ -1,9 +1,11 @@
 import { Hero } from "@/components/sections/home/Hero";
+import Clients from "@/components/sections/home/Clients";
 
 export default function Home() {
   return (
     <>
     <Hero />
+    <Clients/>
     </>
   );
 }
