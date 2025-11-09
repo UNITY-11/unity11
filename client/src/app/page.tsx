37cbfaccbc5cc849b56c45d99d9f3210fa0cf645
ajmal faris <ajmalfaris.me@gmail.com>
2025-11-09 19:31:49 +0530

    chore (home) : deleted unwanted imports and lines

--- client/src/app/page.tsx
+++ client/src/app/page.tsx
@@ -1,11 +1,8 @@
-import Image from "next/image";
 import { Hero } from "@/components/sections/home/Hero";
-import Navbar from "@/components/layout/Navbar";
 
 export default function Home() {
   return (
     <>
-
     <Hero />
     </>
   );
