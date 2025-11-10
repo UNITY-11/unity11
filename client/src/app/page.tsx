eee2c4c16b45a579d976bb4e7dbcadc21c2db25c
ajmal faris <ajmalfaris.me@gmail.com>
2025-11-11 00:39:21 +0530

    feat(services-section): add responsive glassmorphism Services section with Framer Motion animations and tech icons

--- client/src/app/page.tsx
+++ client/src/app/page.tsx
@@ -1,6 +1,7 @@
 import { Hero } from "@/components/sections/home/Hero";
 import Clients from "@/components/sections/home/Clients";
 import AboutSection from "@/components/sections/home/About";
+import ServiceSection from "@/components/sections/services/ServiceSection";
 
 export default function Home() {
   return (
@@ -8,6 +9,7 @@ export default function Home() {
     <Hero />
     <Clients/>
     <AboutSection/>
+    <ServiceSection/>
     </>
   );
 }
