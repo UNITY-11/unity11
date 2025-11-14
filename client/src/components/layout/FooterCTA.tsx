e44c3662832c6b5013f3a6402f871966745e5e47
ajmal faris <ajmalfaris.me@gmail.com>
2025-11-14 14:17:16 +0530

    style (footer cta) : increase the footer padding

--- client/src/components/layout/FooterCTA.tsx
+++ client/src/components/layout/FooterCTA.tsx
@@ -8,7 +8,7 @@ const FooterCTA: React.FC = () => {
         relative w-full  mx-auto
         flex flex-col items-center justify-center
         overflow-hidden rounded-2xl bg-black
-        py-5 text-center px-10"
+        text-center px-10 py-30"
     >
       <div className="w-full flex flex-col justify-center items-center pb-[10vh] z-10">
         {/* Heading */}
