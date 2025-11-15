938ee84e7ed10e166fe2ab756c290a3e0a6a4c8c
ajmal faris <ajmalfaris.me@gmail.com>
2025-11-15 19:56:28 +0530

    chore : mover the service section component to home folder

--- client/src/app/page.tsx
+++ client/src/app/page.tsx
@@ -1,7 +1,7 @@
 import { Hero } from "@/components/sections/home/Hero";
 import Clients from "@/components/sections/home/Clients";
 import AboutSection from "@/components/sections/home/About";
-import ServiceSection from "@/components/sections/services/ServiceSection";
+import ServiceSection from "@/components/sections/home/ServiceSection";
 import WhyChooseUs from "@/components/sections/home/WhyChooseUs";
 import ProcessSection from "@/components/sections/home/ProcessSection";
 import TestimonialSection from "@/components/sections/home/TestimonialSection";
