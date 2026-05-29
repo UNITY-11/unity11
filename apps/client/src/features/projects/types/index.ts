392e0fe5335222b58d5a5c497f2dee88e8680d18
ajmal faris <ajmalfaris.me@gmail.com>
2026-05-29 12:00:00 +0000

    feat: add or update index.ts

---
+++ apps/client/src/features/projects/types/index.ts
@@ -0,0 +1,10 @@
+export interface Project {
+  id: string;
+  tag1: string;
+  tag2: string;
+  title: string;
+  description: string;
+  image: string;
+  bg: string;
+  date: string; // ISO date string for sorting
+}
