f61aef59383ba87e75e1c3bfa32cdb2345cf3aa7
ajmal faris <ajmalfaris.me@gmail.com>
2026-06-04 12:00:00 +0530

    Add apps/client/src/features/home/types/index.ts

---
+++ apps/client/src/features/home/types/index.ts
@@ -0,0 +1,31 @@
+import { LucideIcon } from "lucide-react";
+
+export interface FeaturedProject {
+  id: string;
+  tag1: string;
+  tag2: string;
+  title: string;
+  description: string;
+  image: string;
+  bg: string;
+  index?: number;
+}
+
+export interface Tab {
+  id: string;
+  label: string;
+  isActive: boolean;
+  description?: string;
+}
+
+export interface Reason {
+  icon: LucideIcon;
+  img: string;
+  title: string;
+  desc: string;
+}
+
+export interface ReasonItemProps {
+  item: Reason;
+  index: number;
+}
