b6de53d830d2c829032e36e3a7a4f8f2ae3b1ccf
ajmal faris <ajmalfaris.me@gmail.com>
2026-06-04 12:00:00 +0530

    Add apps/client/src/features/home/data/companies.ts

---
+++ apps/client/src/features/home/data/companies.ts
@@ -0,0 +1,32 @@
+export const companies = [
+  {
+    id: 1,
+    name: "cloudinary",
+    img: "/images/company-images/cloud.svg",
+    nameImg: "/images/company-images/cloudName.svg",
+  },
+  {
+    id: 2,
+    name: "appwrite",
+    img: "/images/company-images/app.svg",
+    nameImg: "/images/company-images/appName.svg",
+  },
+  {
+    id: 3,
+    name: "HOSTINGER",
+    img: "/images/company-images/host.svg",
+    nameImg: "/images/company-images/hostName.svg",
+  },
+  {
+    id: 4,
+    name: "stream",
+    img: "/images/company-images/s.svg",
+    nameImg: "/images/company-images/streamName.svg",
+  },
+  {
+    id: 5,
+    name: "docker.",
+    img: "/images/company-images/dock.svg",
+    nameImg: "/images/company-images/dockerName.svg",
+  },
+];
