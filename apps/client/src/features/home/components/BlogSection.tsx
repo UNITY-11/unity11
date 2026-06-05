b72c6b0dfec67610e2d0244c8bad741b1ee18ddf
ajmal faris <ajmalfaris.me@gmail.com>
2026-06-05 12:00:00 +0530

    Update BlogSection component UI spacing and responsiveness

--- apps/client/src/features/home/components/BlogSection.tsx
+++ apps/client/src/features/home/components/BlogSection.tsx
@@ -70,7 +70,7 @@ export default function BlogSection() {
 
   return (
     <div id="blog" ref={containerRef} className="w-full flex justify-center bg-white">
-      <section className="relative w-full py-20 overflow-hidden bg-black text-white">
+      <section className="relative w-full py-16 md:py-24 lg:py-32 overflow-hidden bg-black text-white">
         {/* Top Notched Shape Cutout */}
         <div className="absolute top-0 left-1/2 -translate-x-1/2 w-[480px] h-[60px] pointer-events-none z-20">
             <svg width="480" height="60" viewBox="0 0 480 60">
@@ -80,14 +80,14 @@ export default function BlogSection() {
                 />
             </svg>
         </div>
-        <div className="max-w-7xl mx-auto md:px-6">
-          <div className="relative flex items-center justify-center mb-10 px-6 md:px-0">
+        <div className="container mx-auto px-4 sm:px-6 lg:px-8 max-w-7xl">
+          <div className="relative flex items-center justify-center mb-12 md:mb-16 lg:mb-20 px-6 md:px-0">
             <h2 className="text-3xl md:text-6xl text-transparent bg-clip-text bg-linear-to-r pb-1 from-[#2052bd] to-[#7fcbe4]">
               Our Blogs
             </h2>
           </div>
 
-        <div className="flex md:grid md:grid-cols-2 lg:grid-cols-3 gap-6 md:gap-8 overflow-x-auto snap-x snap-mandatory [&::-webkit-scrollbar]:hidden [-ms-overflow-style:none] [scrollbar-width:none] px-6 md:px-0 pb-8 md:pb-0">
+        <div className="flex md:grid md:grid-cols-2 lg:grid-cols-3 gap-6 sm:gap-8 lg:gap-10 overflow-x-auto snap-x snap-mandatory [&::-webkit-scrollbar]:hidden [-ms-overflow-style:none] [scrollbar-width:none] px-6 md:px-0 pb-8 md:pb-0">
           {posts.map((post, i) => {
             const cardVariants = getCardVariants(i, isMobile);
 
@@ -135,7 +135,7 @@ export default function BlogSection() {
           })}
         </div>
       </div>
-      <div className="mt-20 right-0 flex justify-center text-blue-500 w-full overflow-hidden px-4">
+      <div className="mt-12 md:mt-16 lg:mt-20 right-0 flex justify-center text-blue-500 w-full overflow-hidden px-4">
         <div className="flex justify-between items-center w-full max-w-md mx-auto">
           <button className="w-10 h-10 shrink-0 rounded-full border-2 flex items-center justify-center border-blue-500 text-blue-500 hover:bg-blue-600 hover:text-white transition-all">
             <IoIosArrowBack className="text-lg" />
