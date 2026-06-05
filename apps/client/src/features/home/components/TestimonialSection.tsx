370440b5fd0b44e40d30f2229e2a57857c669160
ajmal faris <ajmalfaris.me@gmail.com>
2026-06-05 10:10:00 +0530

    feat: add scroll-linked morphing borders to testimonial section

--- apps/client/src/features/home/components/TestimonialSection.tsx
+++ apps/client/src/features/home/components/TestimonialSection.tsx
@@ -4,7 +4,7 @@ import { useRouter } from "next/navigation";
 import { useRef, useState, useEffect } from "react";
 import { IoIosArrowForward, IoIosArrowBack } from "react-icons/io";
 import { StaggerTestimonials } from "@/components/ui/stagger-testimonials";
-import { motion, useScroll, useTransform, useSpring, useMotionTemplate } from "motion/react";
+import { motion, useScroll, useTransform } from "motion/react";
 
 export default function TestimonialSection() {
     const router = useRouter();
@@ -22,31 +22,32 @@ export default function TestimonialSection() {
 
     const { scrollYProgress } = useScroll({
         target: containerRef,
-        offset: ["0 1", "0 0"]
+        offset: ["start 80%", "start 20%"]
     });
 
-    const smoothProgress = useSpring(scrollYProgress, {
-        stiffness: 100,
-        damping: 30,
-        restDelta: 0.001
-    });
-
-    const targetProgress = isMobile ? 0.5 : 1;
-    const startWidth = isMobile ? 90 : 50;
-    const startRadius = isMobile ? 60 : 300;
-
-    const widthVal = useTransform(smoothProgress, [0, targetProgress], [startWidth, 100]);
-    const width = useMotionTemplate`${widthVal}%`;
-
-    const radiusVal = useTransform(smoothProgress, [0, targetProgress], [startRadius, 0]);
-    const borderRadius = useMotionTemplate`${radiusVal}px`;
+    const notchPath = useTransform(
+        scrollYProgress,
+        [0, 1],
+        [
+            "M 0 0 C 48 0, 72 0, 144 0 L 336 0 C 408 0, 432 0, 480 0 Z",
+            "M 0 0 C 48 0, 72 56, 144 56 L 336 56 C 408 56, 432 0, 480 0 Z"
+        ]
+    );
 
     return (
-        <div id="testimonials" ref={containerRef} className="w-full flex justify-center pt-10 bg-black">
-            <motion.section 
-                style={{ width, borderRadius }}
-                className="relative text-blue-500 py-20 overflow-hidden bg-white"
+        <div id="testimonials" ref={containerRef} className="w-full flex justify-center bg-black">
+            <section 
+                className="relative w-full text-blue-500 py-20 overflow-hidden bg-white"
             >
+                {/* Top Notched Shape Cutout */}
+                <div className="absolute top-0 left-1/2 -translate-x-1/2 w-[480px] h-[60px] pointer-events-none z-20">
+                    <svg width="480" height="60" viewBox="0 0 480 60">
+                        <motion.path 
+                            d={notchPath as any}
+                            fill="black" 
+                        />
+                    </svg>
+                </div>
                 <div className="container max-w-7xl mx-auto text-start px-6">
                 <div className="relative w-full flex flex-col justify-center items-center text-start">
                     <h2 className="text-4xl sm:text-6xl text-[#2052bd]">
@@ -88,7 +89,7 @@ export default function TestimonialSection() {
                     </button>
                 </div>
             </div>
-            </motion.section>
+            </section>
         </div>
     );
 }
