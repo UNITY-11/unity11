46b69b6634d84883d513bf377a5295c45e1297aa
ajmal faris <ajmalfaris.me@gmail.com>
2026-06-06 12:00:00 +0000

    Update WhyChooseUs section

--- apps/client/src/features/home/components/WhyChooseUs.tsx
+++ apps/client/src/features/home/components/WhyChooseUs.tsx
@@ -14,11 +14,12 @@ import {
 } from "lucide-react";
 import React from "react";
 
-
 import { Reason, ReasonItemProps } from "../types";
+import { useIsMobile } from "@/hooks/use-mobile";
 
 const ReasonItem: React.FC<ReasonItemProps> = ({ item, index }) => {
   const isImageLeft = index % 2 === 0;
+  const isMobile = useIsMobile();
 
   const textAnimation = (index: number) => ({
     initial: {opacity: 0, y:50, x: index % 2 === 0 ? 30 : -30 },
@@ -40,6 +41,7 @@ const ReasonItem: React.FC<ReasonItemProps> = ({ item, index }) => {
     >
       <motion.div
         {...imageAnimation(index)}
+        viewport={{ once: isMobile }}
         className="w-full md:w-1/2 p-4 flex justify-center relative group"
       >
         <div className="relative w-full max-w-sm aspect-square flex items-center justify-center cursor-crosshair">
@@ -51,7 +53,7 @@ const ReasonItem: React.FC<ReasonItemProps> = ({ item, index }) => {
         </div>
       </motion.div>
 
-      <motion.div {...textAnimation(index)} className="w-full md:w-1/2 md:p-8">
+      <motion.div {...textAnimation(index)} viewport={{ once: isMobile }} className="w-full md:w-1/2 md:p-8">
         <div className="flex items-center justify-center text-center mb-4">
           <h3 className="text-3xl md:text-5xl text-transparent text-nowrap pb-2 bg-clip-text bg-linear-to-r from-blue-700 to-cyan-500 text-center">
             {item.title}
@@ -66,6 +68,7 @@ const ReasonItem: React.FC<ReasonItemProps> = ({ item, index }) => {
 };
 
 export default function WhyChooseUs() {
+  const isMobile = useIsMobile();
   const reasons: Reason[] = [
     {
       icon: CodeSquare,
@@ -118,6 +121,7 @@ export default function WhyChooseUs() {
       <motion.div
         initial={{ y: 50 }}
         whileInView={{ y: 0 }}
+        viewport={{ once: isMobile }}
         transition={{ duration: 1 }}
         className="absolute top-[340px] sm:top-72 md:top-60 left-1/2 -translate-x-1/2 w-[200vw] md:w-[150vw] h-[200vw] md:h-[150vw] rounded-full scale-120 bg-linear-to-r from-blue-700 to-cyan-400 pt-3 z-0 pointer-events-none"
       >
@@ -142,6 +146,7 @@ export default function WhyChooseUs() {
         <motion.div
           initial={{ opacity: 0, y: 30 }}
           whileInView={{ opacity: 1, y: 0 }}
+          viewport={{ once: isMobile }}
           transition={{ duration: 1 }}
           className="text-center mb-12 md:mb-16 lg:mb-20"
         >
